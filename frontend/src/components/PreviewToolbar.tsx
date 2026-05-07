066b00f3d18b1de41a4f0df5c4e26990c7d94482
Swati <sg7254@srmist.edu.in>
2026-05-07 23:27:13 +0530

    PreviewToolbar.tsx

---
+++ frontend/src/components/PreviewToolbar.tsx
@@ -0,0 +1,9 @@
+// Floating Preview Toolbar - May 7, 2026
+// Provides template switcher, color picker, font selector, PDF download
+
+import { TemplateSelector } from './TemplateSelector';
+// ... other imports
+
+export const PreviewToolbar = ({ onTemplateChange, onColorChange, onFontChange, onPDF }) => {
+  return ( /* toolbar with glassmorphism, gold accents */ );
+};
